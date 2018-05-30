301525886d5c5db9c8630774d433a50b8da61483
payeer1982 <39153086+payeer1982@users.noreply.github.com>
2018-05-30 11:16:17 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -93,8 +93,8 @@ public:
 		base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
 		base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        vSeeds.push_back(CDNSSeedData("0",  "194.67.195.72"));
-        vSeeds.push_back(CDNSSeedData("1",  "194.67.195.43"));
+        vSeeds.push_back(CDNSSeedData("209.250.248.103","209.250.248.103"));
+        vSeeds.push_back(CDNSSeedData("85.53.81.75","85.53.81.75"));
 		convertSeeds(vFixedSeeds, pnSeed, ARRAYLEN(pnSeed), nDefaultPort);
 
 		nPoolMaxTransactions = 3;
