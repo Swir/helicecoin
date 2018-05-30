c5528c336511c25ea4a5e7855e6a062edfc8b49e
payeer1982 <39153086+payeer1982@users.noreply.github.com>
2018-05-30 11:14:42 +0200

    Update chainparamsseeds.h

--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -7,8 +7,8 @@
 // IPv4 as well as onion addresses are wrapped inside a IPv6 address accordingly.
 static const unsigned int pnSeed[] =
 {
-//   0x63ecd7e,
-//   0xc61b61ac,
+   0x30b84965a7,
+   0x519357f,
 //   0xc632f359,
 };
 
